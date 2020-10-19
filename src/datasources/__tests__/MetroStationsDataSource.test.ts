d94c3b0c21d1a0ca5bd7a7a8117c58198ab798a9
Albert Alises <albert.alises@kiwi.com>
2020-10-19 10:21:27 +0200

    test(datasource): add test for getAllStations response

--- src/datasources/__tests__/MetroStationsDataSource.test.ts
+++ src/datasources/__tests__/MetroStationsDataSource.test.ts
@@ -13,7 +13,10 @@ dataSource.get = mockGet;
 
 describe("MetroStationsDataSource", () => {
   it("Correctly looks up the stations from the API", async () => {
-    await dataSource.getAllStations();
+    mockGet.mockReturnValueOnce(mockMetroStationsAPIResponse);
+
+    const res = await dataSource.getAllStations();
+    expect(res).toEqual(mockMetroStationsResponse);
 
     expect(mockGet).toBeCalledWith("estacions", {
       app_id: "testAppId",
@@ -22,15 +25,11 @@ describe("MetroStationsDataSource", () => {
   });
 
   it("Correctly parses an station API data to the schema format", () => {
-    expect(dataSource.metroStationReducer(data as any)).toEqual({
-      id: "ESTACIONS.6660935",
-      lines: ["L10N"],
-      location: {
-        latitude: 41.442817,
-        longitude: 2.224737,
-      },
-      name: "La Salut",
-    });
+    expect(
+      dataSource.metroStationReducer(
+        mockMetroStationsAPIResponse.features[0] as any
+      )
+    ).toEqual(mockMetroStationsResponse.stations[0]);
   });
 
   test.each([
@@ -43,18 +42,72 @@ describe("MetroStationsDataSource", () => {
   });
 });
 
-const data = {
-  type: "Feature",
-  id: "ESTACIONS.6660935",
-  geometry: {
-    type: "Point",
-    coordinates: [2.224737, 41.442817],
+const mockMetroStationsAPIResponse = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      id: "ESTACIONS.6660935",
+      geometry: {
+        type: "Point",
+        coordinates: [2.224737, 41.442817],
+      },
+      geometry_name: "GEOMETRY",
+      properties: {
+        CODI_GRUP_ESTACIO: 6660935,
+        NOM_ESTACIO: "La Salut",
+        PICTO: "L10N",
+        DATA: "2020-10-18Z",
+      },
+    },
+    {
+      type: "Feature",
+      id: "ESTACIONS.6660525",
+      geometry: {
+        type: "Point",
+        coordinates: [2.181497, 41.41506],
+      },
+      geometry_name: "GEOMETRY",
+      properties: {
+        CODI_GRUP_ESTACIO: 6660525,
+        NOM_ESTACIO: "Camp de l'Arpa",
+        PICTO: "L5",
+        DATA: "2020-10-18Z",
+      },
+    },
+  ],
+  totalFeatures: 135,
+  numberMatched: 2,
+  numberReturned: 2,
+  timeStamp: "2020-10-19T06:44:29.076Z",
+  crs: {
+    type: "name",
+    properties: {
+      name: "urn:ogc:def:crs:EPSG::4326",
+    },
   },
-  geometry_name: "GEOMETRY",
-  properties: {
-    CODI_GRUP_ESTACIO: 6660935,
-    NOM_ESTACIO: "La Salut",
-    PICTO: "L10N",
-    DATA: "2020-10-11Z",
-  },
+};
+
+const mockMetroStationsResponse = {
+  numberOfStations: 2,
+  stations: [
+    {
+      id: "ESTACIONS.6660935",
+      lines: ["L10N"],
+      location: {
+        latitude: 41.442817,
+        longitude: 2.224737,
+      },
+      name: "La Salut",
+    },
+    {
+      id: "ESTACIONS.6660525",
+      lines: ["L5"],
+      location: {
+        latitude: 41.41506,
+        longitude: 2.181497,
+      },
+      name: "Camp de l'Arpa",
+    },
+  ],
 };
